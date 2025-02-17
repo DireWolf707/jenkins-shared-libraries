37d500db4e0c22ef5d1a421216aeef01ab6eb84c
Direwolf <rahulsood707@gmail.com>
2025-02-18 00:39:38 +0530

    improved cleanup

--- vars/docker_cleanup.groovy
+++ vars/docker_cleanup.groovy
@@ -1,5 +1,3 @@
 def call(String projectName, String projectTag) {
-  withCredentials([usernamePassword(credentialsId: 'docker-login', usernameVariable: 'dockerUsername', passwordVariable: '')]) {
-    sh "docker rmi ${dockerUsername}/${projectName}:${projectTag}"
-  }
+  sh "docker image prune -f"
 }
