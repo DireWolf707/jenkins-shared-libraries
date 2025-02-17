f26618c9abc022d28989f06122d27d2097d54e96
Direwolf <rahulsood707@gmail.com>
2025-02-17 23:22:03 +0530

    fixed username

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,7 @@
 def call(String projectName, String projectTag) {
   withCredentials([usernamePassword(credentialsId: 'docker-login', passwordVariable: 'dockerPassword', usernameVariable: 'dockerUsername')]) {
-      sh "echo "${dockerPassword}" | docker login -u ${dockerUsername} --password-stdin"
+      sh "echo ${dockerPassword} | docker login -u ${dockerUsername} --password-stdin"
       sh "docker image tag ${projectName}:${projectTag} ${env.dockerUsername}/${projectName}:${projectTag}"
-      sh "docker push ${username}/${projectName}:${projectTag}"
+      sh "docker push ${dockerUsername}/${projectName}:${projectTag}"
   }
 }
