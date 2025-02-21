430f03cb9c5f9d95acef8ffdf756ff2578819557
Direwolf <rahulsood707@gmail.com>
2025-02-21 20:31:04 +0530

    fixed

--- vars/git_clone.groovy
+++ vars/git_clone.groovy
@@ -1,7 +1,3 @@
 def call(String url, String branch) {
-    withCredentials([usernamePassword(credentialsId: 'github-app',
-                                          usernameVariable: 'GITHUB_APP',
-                                          passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
-        git credentialsId: "${GITHUB_ACCESS_TOKEN}", url: "${url}", branch: "${branch}"
-    }
+    git credentialsId: 'github-app', url: "${url}", branch: "${branch}"
 }
